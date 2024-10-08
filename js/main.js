b1e663b5e0b53c698bdcb14ee546e4800a31cacf
Tacoman777 <166954649+Tacoman777@users.noreply.github.com>
2024-10-08 14:38:15 -0400

    header and image

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.getElementById("h1").textContent = `Trip Destination Check List for Delaware`;
+
+var img = document.createElement("img");
+img.src = "images/DE.jpg";
+var src = document.getElementById("lf");
+src.appendChild(img);
+function list() {
+    const checks = ["Food", "Water", "Jacket for Winter", "Clothes", "Phone", "Luggage", "Money", "Wallet", "Car for Transportation" ];
+    
+}
+list();
+
+
+
+
