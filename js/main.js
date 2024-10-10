d8792d9cef6950081a7558d59ff0939f780065bb
Tacoman777 <166954649+Tacoman777@users.noreply.github.com>
2024-10-10 13:02:49 -0400

    week 5 in progress

--- js/main.js
+++ js/main.js
@@ -4,9 +4,29 @@ var img = document.createElement("img");
 img.src = "images/DE.jpg";
 var src = document.getElementById("lf");
 src.appendChild(img);
+
 function list() {
-    const checks = ["Food", "Water", "Jacket for Winter", "Clothes", "Phone", "Luggage", "Money", "Wallet", "Car for Transportation" ];
+
+    var itemsAvailable = document.getElementById('div-list');
+    var checks = new Array();
+    var checks = ["Food", "Water", "Jacket for Winter", "Clothes", "Phone", "Luggage", "Money", "Wallet", "Car for Transportation" ];
     
+
+    var item, p, br;
+
+   for(var count in checks)
+    {
+      item=document.createElement("input");   
+      item.value=(checks[count] + '</br>');
+      item.type="checkbox";
+      item.id="item" + count;
+      p =document.createElement("span");
+      p.innerHTML = checks[count] + ": ";
+      br =document.createElement("br");
+      itemsAvailable.appendChild(p);
+      itemsAvailable.appendChild(item);
+      itemsAvailable.appendChild(br);
+   }
 }
 list();
 
