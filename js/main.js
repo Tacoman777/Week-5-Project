d48d3c470f30b24860601a1b04293aaf16ef0e8a
Tacoman777 <166954649+Tacoman777@users.noreply.github.com>
2024-10-15 12:16:54 -0400

    week 5 complete

--- js/main.js
+++ js/main.js
@@ -1,35 +1,45 @@
 document.getElementById("h1").textContent = `Trip Destination Check List for Delaware`;
 
 var img = document.createElement("img");
-img.src = "images/DE.jpg";
+img.src = "images/delaware-img.jpeg";
 var src = document.getElementById("lf");
 src.appendChild(img);
+
+var checkedItems = 0;
 
 function list() {
 
     var itemsAvailable = document.getElementById('div-list');
     var checks = new Array();
-    var checks = ["Food", "Water", "Jacket for Winter", "Clothes", "Phone", "Luggage", "Money", "Wallet", "Car for Transportation" ];
-    
-
+    var checks = [" Food", " Water", " Jacket", " Car", " Phone", " Luggage", " Money", " Wallet", " Shoes" ];
     var item, p, br;
 
-   for(var count in checks)
+    for(var count in checks)
     {
-      item=document.createElement("input");   
-      item.value=(checks[count] + '</br>');
-      item.type="checkbox";
-      item.id="item" + count;
-      p =document.createElement("span");
-      p.innerHTML = checks[count] + ": ";
-      br =document.createElement("br");
-      itemsAvailable.appendChild(p);
-      itemsAvailable.appendChild(item);
-      itemsAvailable.appendChild(br);
-   }
+        item = document.createElement("input");   
+        item.value=(checks[count] + '</br>');
+        item.type="checkbox";
+        item.id="item" + count;
+        item.onclick = (function(item) {
+            return function() {
+                validate(item);
+            };
+        })(item);
+        p = document.createElement("span");
+        p.innerHTML = checks[count];
+        br = document.createElement("br");
+        itemsAvailable.appendChild(item);
+        itemsAvailable.appendChild(p);
+        itemsAvailable.appendChild(br);
+    }
+}
+
+function validate(checkbox) {
+    if (checkbox.checked) {
+        checkedItems += 1;
+    } else {
+        checkedItems -= 1;
+    }
+    document.getElementById("tally").innerHTML = `Checked Items: ` + checkedItems;
 }
 list();
-
-
-
-
